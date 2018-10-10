1c66aebfcb2506b1b8b58de9df03fffc7d6a8ebe
qiansongke <455988586@qq.com>
2018-10-10 15:34:52 +0800

    v0.0.1

--- static/wap/js/index.js
+++ static/wap/js/index.js
@@ -1,13 +1,7 @@
 $(document).ready(function () {
-    (function () {
-        alert(1);
-        var newstitle = $(".news ul:nth-of-type(3) li div p");
-        //news判断新闻标题长度并且给予省略号
-        if (newstitle.text().length >= 50) {
-            alert(true)
-            newstitle.text(newstitle.text() + '...')
-        } else {
-            alert(false)
-        }
-    })
+    var newstitle = $(".news ul:nth-of-type(3) li div p");
+    //news判断新闻标题长度并且给予省略号
+    if (newstitle.text().length >= 20) {
+        newstitle.text(newstitle.text().substring(0,20) + '...')
+    }
 })
